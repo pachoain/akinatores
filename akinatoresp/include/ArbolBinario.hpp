38c5897855b2a290a4ea81e29a7aeaeae5220499
projetlogiciel <fxl.leclerc@yahoo.com>
2019-05-16 11:25:37 +0200

    ajout attibuts et fonctions de la classe ArbolBinario

--- akinatoresp/include/ArbolBinario.hpp
+++ akinatoresp/include/ArbolBinario.hpp
@@ -4,13 +4,23 @@
 
 class ArbolBinario
 {
+    private:
+        nodo raiz;
+
+
     public:
         ArbolBinario();
         virtual ~ArbolBinario();
 
+        void setRaiz(nodo raiz);
+        nodo getRaiz();
+        void anadirNodo(nodo nodo, nodo padre, bool respuesta);
+        void borrarNodo(nodo nodo);
+
+
     protected:
 
-    private:
+
 };
 
 #endif // ARBOLBINARIO_HPP
--- akinatoresp/src/ArbolBinario.cpp
+++ akinatoresp/src/ArbolBinario.cpp
@@ -3,10 +3,33 @@
 
 ArbolBinario::ArbolBinario()
 {
-    //ctor
+    this.raiz = NULL;
+}
+
+ArbolBinario::ArbolBinario(nodo nod)
+{
+    this.raiz = nod;
 }
 
 ArbolBinario::~ArbolBinario()
 {
-    //dtor
+    delete this.raiz;
 }
+
+void ArbolBinario::setRaiz(nodo nod){
+    this.raiz = nod;
+}
+
+nodo ArbolBinario::getRaiz(){
+    return this.raiz;
+}
+
+void ArbolBinario::anadirNodo(nodo nodo, nodo padre, bool respuesta){
+    //metodo para anadir un nodo
+
+}
+
+void ArbolBinarioborrarNodo(nodo nodo){
+    // metodo para borrar un nodo en el arbol
+}
+
