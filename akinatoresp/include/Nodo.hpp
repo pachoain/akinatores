4a21e0d241fc7fe8e02be6df61b44e3c5c3c8ca0
dekolhi56 <dekolhi56@gmail.com>
2019-05-16 11:32:03 +0200

    Adding getters and setters and the constructor

--- akinatoresp/include/Nodo.hpp
+++ akinatoresp/include/Nodo.hpp
@@ -5,14 +5,29 @@ class Nodo
 {
 
 public :
-    Nodo();
+    Nodo(int ID_n, string Name_n, int id_from_n, bool response_n, int ID_son_l_n, int ID_son_r_n);
     ~Nodo();
+
     bool get_IsNode();
+    int get_ID();
+    string get_Name();
+    int get_UpperNode();
+    int get_IDsonL();
+    int get_IDsonR();
+
+    void set_IDsonL(int Lson);
+    void set_IDsonR(int Rson);
+    void set_UpperNode(int UN);
 
 protected :
 
 private :
     bool IsNode;
+    int ID;
+    string Name;
+    int UpperNode;
+    int ID_son_l;
+    int ID_son_r;
 
 };
 
--- akinatoresp/src/Nodo.cpp
+++ akinatoresp/src/Nodo.cpp
@@ -1,9 +1,17 @@
 #include "Nodo.hpp"
 
 
-Nodo::Nodo()
+Nodo::Nodo(int ID_n, string Name_n, int id_from_n, bool response_n, int ID_son_l_n, int ID_son_r_n)
 {
-
+    ID=ID_n;
+    Name=Name_n;
+    UpperNode=id_from_n;
+    IsNode=response_n;
+    if (IsNode)
+        {
+            ID_son_l=ID_son_l_n;
+            ID_son_r=ID_son_r_n;
+        }
 }
 
 Nodo::~Nodo()
@@ -11,7 +19,55 @@ Nodo::~Nodo()
 
 }
 
+//Tells if it has sons
 bool Nodo::get_IsNode()
 {
     return(IsNode);
 }
+
+//Return the Name/Question
+string Nodo::get_Name()
+{
+    return(Name);
+}
+
+//ID of the node, unique
+int Nodo::get_ID()
+{
+    return(ID);
+}
+
+//getter and setter for the father of this node
+void Nodo::set_UpperNode(int UN)
+{
+    UpperNode=UN;
+}
+
+int Nodo::get_UpperNode()
+{
+    return(UpperNode);
+}
+
+//getter and setter for the sons of this node
+void Nodo::set_IDsonL(int Lson)
+{
+    IsNode=1;
+    ID_son_l=Lson;
+}
+
+int Nodo::get_IDsonL()
+{
+    return(ID_son_l)
+}
+
+void Nodo::set_IDsonR(int Rson)
+{
+    IsNode=1;
+    ID_son_r=Rson;
+}
+
+int Nodo::get_IDsonR()
+{
+    return(ID_son_r)
+}
+
